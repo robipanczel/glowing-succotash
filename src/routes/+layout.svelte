6e13c35c849828915e6e5177a0db2f08c6a43053
Róbert Pánczél <robi.panczel@gmail.com>
2023-06-24 18:07:26 +0200

    Install and setup skeleton and tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
 </script>
 
 <div class="app">
